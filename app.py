ba2012481cf18b9e014a06b323e19532b04a8c11
nmy2103 <yamane.n@northeastern.edu>
2023-08-09 17:44:19 -0400

    added background callback manager

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from dash import Dash
+from os import environ
+from dash import Dash, DiskcacheManager, CeleryManager
 from heartview.layout import layout
 from heartview.callbacks import get_callbacks
 import dash_bootstrap_components as dbc
 
+if 'REDIS_URL' in environ:
+    from celery import Celery
+    celery_app = Celery(__name__, broker = environ['REDIS_URL'],
+                        backend = environ['REDIS_URL'])
+    background_callback_manager = CeleryManager(celery_app)
+else:
+    import diskcache
+    cache = diskcache.Cache('./cache')
+    background_callback_manager = DiskcacheManager(cache)
+
 app = Dash(
     __name__,
     update_title = None,
-    external_stylesheets = [dbc.themes.LUX, dbc.icons.FONT_AWESOME]
+    external_stylesheets = [dbc.themes.LUX, dbc.icons.FONT_AWESOME],
+    background_callback_manager = background_callback_manager
 )
 app.title = 'HeartView Dashboard'
 app.layout = layout
