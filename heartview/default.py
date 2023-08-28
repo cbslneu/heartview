7df808993268cfa3dca8fc53483375e36d5828aa
nmy2103 <yamane.n@northeastern.edu>
2023-08-28 15:29:12 -0400

    cleaned up functions

--- heartview/default.py
+++ heartview/default.py
@@ -2,6 +2,7 @@ from os import listdir, path, remove, makedirs
 from pathlib import Path
 from shutil import rmtree
 from zipfile import ZipFile
+from dash import dcc
 import dash_bootstrap_components as dbc
 import pandas as pd
 import numpy as np
@@ -45,7 +46,7 @@ def _get_configs():
     else:
         return []
 
-def _create_configs(fs, seg_size, filters: list or None, headers: dict):
+def _create_configs(device, fs, seg_size, filters: list or None, headers: dict):
     """Create a JSON-formatted configuration file of user
     SQA parameters.
 
@@ -64,7 +65,8 @@ def _create_configs(fs, seg_size, filters: list or None, headers: dict):
     """
 
     # Save user configuration
-    configs = {"sampling rate": fs,
+    configs = {"device": device,
+               "sampling rate": fs,
                "segment size": seg_size,
                "filters": filters,
                "headers": headers}
@@ -74,14 +76,9 @@ def _create_configs(fs, seg_size, filters: list or None, headers: dict):
 
     return json_object
 
-def _export_configs(json_object, filename):
-    """Export SQA parameters to a JSON file."""
-    with open(f'./configs/{filename}.json', 'w') as outfile:
-        outfile.write(json_object)
-
 def _load_config(filename):
     """Load a JSON configuration file into a dictionary."""
-    cfg = open(f'./configs/{filename}')
+    cfg = open(filename)
     configs = json.load(cfg)
     return configs
 
@@ -153,7 +150,7 @@ def segment_data(data, fs, seg_size):
     return df
 
 
-def blank_fig(context):
+def _blank_fig(context):
     """Display the default blank figure."""
     fig = go.Figure(go.Scatter(x = [], y = []))
     fig.update_layout(template = None,
@@ -181,7 +178,7 @@ def blank_fig(context):
                            x = 0.5, y = 0.5, showarrow = False)
     return fig
 
-def blank_table():
+def _blank_table():
     """Display the default blank table."""
     summary = pd.DataFrame({
         'Signal Quality Metrics': ['Segments with Missing Beats',
@@ -380,241 +377,3 @@ def plot_ibi_from_ecg(df, x, y, segment, n_segments):
     fig.update_yaxes(
         title_standoff = 10)
     return fig
-
-# ========================= DEMO PLOTTING FUNCTIONS ==========================
-def demo_table(i, person, condition, seg_size):
-    """Display the demo table."""
-    df = pd.read_csv(f'INSAR/demo/{i}_{person}_{condition}_SQA_{seg_size}.csv')
-    missing_n = len(df.loc[df.Missing > 0])
-    invalid_n = len(df.loc[df.Invalid == 1])
-    avg_missing = '{0:.2f}%'.format((df['% Missing']).mean())
-    durations = pd.read_csv('INSAR/demo/file_durations.csv')
-    dur = durations[f'{i}-{person}-{condition}'].item()
-    summary = pd.DataFrame({
-        'Signal Quality Metrics': ['Segments with Missing Beats',
-                       'Segments with Invalid Beats',
-                       'Average % Missing Beats/Segment',
-                       'File Duration (min)'],
-        '': [missing_n, invalid_n, avg_missing, round(dur, 2)]})
-    summary.set_index('Signal Quality Metrics', inplace = True)
-    return dbc.Table.from_dataframe(
-        summary,
-        index = True,
-        className = 'segmentTable',
-        striped = False,
-        hover = False,
-        bordered = False)
-
-def demo_table2(i):
-    """Display the demo table."""
-    df = pd.read_csv(f'INSAR/demo/{i}_SQA.csv')
-    missing_n = len(df.loc[df.Missing > 0])
-    invalid_n = len(df.loc[df.Invalid == 1])
-    avg_missing = '{0:.2f}%'.format((df['% Missing']).mean())
-    dur = 119.5
-    summary = pd.DataFrame({
-        'Signal Quality Metrics': ['Segments with Missing Beats',
-                       'Segments with Invalid Beats',
-                       'Average % Missing Beats/Segment',
-                       'File Duration (min)'],
-        '': [missing_n, invalid_n, avg_missing, round(dur, 2)]})
-    summary.set_index('Signal Quality Metrics', inplace = True)
-    return dbc.Table.from_dataframe(
-        summary,
-        index = True,
-        className = 'segmentTable',
-        striped = False,
-        hover = False,
-        bordered = False)
-
-def demo_fig(i, person, condition, seg_size):
-    """Display the demo expected-to-missing beats chart."""
-    df = pd.read_csv(f'INSAR/demo/{i}_{person}_{condition}_SQA_{seg_size}.csv')
-    # df['% Missing'] = df['% Missing'] * 100  # if not %
-    valid = df[df.Invalid == 0].index.values
-    fig = go.Figure(data = [
-        go.Bar(
-            name = 'Expected Beats',
-            x = df.loc[valid, 'Segment'],
-            y = df.loc[valid, 'Expected'],
-            marker = dict(color = '#f2ce4b'),
-            hovertemplate = '<b>Segment %{x}:</b> %{y:.2f} '
-                            'expected beats<extra></extra>'),
-        go.Bar(
-            name = 'Missing Beats',
-            x = df['Segment'],
-            y = df['Missing'],
-            opacity = 0.7, marker = dict(color = '#fa2a0a'),
-            hovertemplate = '<b>Segment %{x}:</b> %{y:.2f} '
-                            'missing beats<extra></extra>')
-    ])
-    invalid = df.index[df.Invalid == 1]
-    fig.add_traces(
-        go.Bar(
-            x = df.loc[invalid, 'Segment'],
-            y = df.loc[invalid, 'Expected'],
-            name = 'Invalid Segment',
-            width = 0.8,
-            marker_color = '#e8e8e8',
-            hovertemplate = '<b>Segment %{x}:</b> %{y:.2f} '
-                            'expected beats<extra></extra>'))
-    fig.update_layout(
-        title = f'{i} {person.title()} ({seg_size}-Sec Windows)',
-        xaxis_title = 'Segment Number',
-        yaxis_title = 'Number of Beats',
-        xaxis = dict(tickmode = 'linear', dtick = 1),
-        margin = dict(t = 80, r = 20),
-        barmode = 'overlay',
-        template = 'plotly_white',
-        legend = dict(orientation = 'h',
-                      yanchor = 'bottom', y = 1.0,
-                      xanchor = 'right', x = 1.0)
-    )
-    return fig
-
-def demo_fig2(i):
-    """Display the demo expected-to-missing beats chart."""
-    df = pd.read_csv(f'INSAR/demo/{i}_SQA.csv')
-    valid = df[df.Invalid == 0].index.values
-    fig = go.Figure(data = [
-        go.Bar(
-            name = 'Expected Beats',
-            x = df.loc[valid, 'Segment'],
-            y = df.loc[valid, 'Expected'],
-            marker = dict(color = '#f2ce4b'),
-            hovertemplate = '<b>Segment %{x}:</b> %{y:.2f} '
-                            'expected beats<extra></extra>'),
-        go.Bar(
-            name = 'Missing Beats',
-            x = df['Segment'],
-            y = df['Missing'],
-            opacity = 0.7, marker = dict(color = '#fa2a0a'),
-            hovertemplate = '<b>Segment %{x}:</b> %{y:.2f} '
-                            'missing beats<extra></extra>')
-    ])
-    invalid = df.index[df.Invalid == 1]
-    fig.add_traces(
-        go.Bar(
-            x = df.loc[invalid, 'Segment'],
-            y = df.loc[invalid, 'Expected'],
-            name = 'Invalid Segment',
-            width = 0.8,
-            marker_color = '#e8e8e8',
-            hovertemplate = '<b>Segment %{x}:</b> %{y:.2f} '
-                            'expected beats<extra></extra>'))
-    fig.update_layout(
-        title = f'{i}',
-        xaxis_title = 'Segment Number',
-        yaxis_title = 'Number of Beats',
-        xaxis = dict(tickmode = 'linear', dtick = 5),
-        margin = dict(t = 80, r = 20),
-        barmode = 'overlay',
-        template = 'plotly_white',
-        legend = dict(orientation = 'h',
-                      yanchor = 'bottom', y = 1.0,
-                      xanchor = 'right', x = 1.0)
-    )
-    return fig
-
-def demo_ibi_plot(i, person, condition, seg_num, seg_n):
-    """Display the INSAR demo IBI plot."""
-    df = pd.read_csv(f'INSAR/demo/{i}_{person}_{condition}_ECG_plotting.csv')
-    df['Timestamp'] = pd.to_datetime(df['Timestamp'])
-    seg = 1
-    chunk = int(1024 * 60)
-    for n in range(0, len(df), chunk):
-        df.loc[n:(n + chunk - 1), 'Segment'] = seg
-        seg += 1
-    peak_loc = df.loc[df['Peak'] == 1].index.values
-    ts = df.loc[peak_loc, ['Timestamp', 'Segment']].reset_index(
-        drop = True)
-    segs = []
-    ibis = []
-    for t in range(1, len(ts)):
-        seg = ts.loc[t, 'Segment'].item()
-        segs.append(seg)
-        nn_s = (ts.loc[t, 'Timestamp'] - ts.loc[
-            t - 1, 'Timestamp']).total_seconds()
-        nn_ms = nn_s * 1000
-        ibis.append(nn_ms)
-    ibi = pd.DataFrame({'Segment': segs, 'Timestamp': ts.loc[1:, 'Timestamp'],
-                        'IBI': ibis}).reset_index(drop = True)
-    fig = plot_ibi_from_ecg(ibi, 'Timestamp', 'IBI', seg_num, seg_n)
-    return fig
-
-def demo_ibi_plot2(bvp, ibi, seg_num, seg_n):
-    """Display the INSAR demo IBI plot."""
-    # bvp['Timestamp'] = pd.to_datetime(bvp['Timestamp'])
-    # ibi['Timestamp'] = pd.to_datetime(ibi['Timestamp'])
-    bvp['Segment'] = np.nan
-    seg = 1
-    chunk = int(64 * 60)
-    for n in range(0, len(bvp), chunk):
-        bvp['Segment'].iloc[n:(n + chunk - 1)] = seg
-        # bvp.loc[n:(n + chunk - 1), 'Segment'] = seg
-        seg += 1
-    for n in range(len(ibi)):
-        ts = ibi.loc[n, 'Timestamp']
-        seg = bvp[(bvp.Timestamp >= ts)].iloc[0]['Segment']
-        ibi.loc[n, 'Segment'] = seg.item()
-    ibi['IBI'] = ibi['IBI'] * 1000
-    fig = plot_ibi_from_ecg(ibi, 'Timestamp', 'IBI', seg_num, seg_n)
-    return fig
-
-def demo_ecg_plot(i, person, condition, seg_num, seg_n):
-    """Display the INSAR demo ECG plot."""
-    person = person.upper()
-    ecg = pd.read_csv(f'INSAR/demo/{i}_{person}_{condition}_ECG_plotting.csv')
-    fig = plot_signal(ecg, 'Timestamp',
-                      ['mV', 'Filtered'],
-                      1024, seg_num, seg_n, 'Peak')
-    fig.update_layout(
-        height = 300,
-        margin = dict(l = 10, r = 10, b = 50, t = 30, pad = 10),
-        yaxis_title = 'ECG'
-    )
-    return fig
-
-def demo_acc_plot(i, person, condition, seg_num, seg_n):
-    """Display the INSAR demo ACC plot."""
-    acc = pd.read_csv(f'INSAR/demo/{i}_{person}_{condition}_ACC.csv')
-    acc['Magnitude'] = np.sqrt(
-        acc[['X', 'Y', 'Z']].apply(lambda x: x ** 2).sum(axis = 1))
-    fig = plot_signal(acc, 'Timestamp', 'Magnitude', 32, seg_num, seg_n)
-    fig.update_layout(
-        yaxis_title = 'm/s<sup>2</sup>',
-        xaxis_title = 'Timestamp',
-        height = 300,
-        margin = dict(l = 10, r = 10, b = 50, t = 30, pad = 10),
-    )
-    fig.update_traces(
-        marker = dict(color = '#63b068')
-    )
-    return fig
-
-def demo_acc_plot2(i, seg_num, seg_n):
-    """Display the INSAR WESAD demo ACC plot."""
-    acc = pd.read_csv(f'INSAR/demo/{i}_ACC.csv')
-    fig = plot_signal(acc, 'Timestamp', 'magnitude', 32, seg_num, seg_n)
-    fig.update_layout(
-        yaxis_title = 'm/s<sup>2</sup>',
-        xaxis_title = 'Timestamp',
-        height = 300,
-        margin = dict(l = 10, r = 10, b = 50, t = 30, pad = 10),
-    )
-    fig.update_traces(
-        marker = dict(color = '#63b068')
-    )
-    return fig
-
-def demo_bvp_plot(bvp, fs, seg_num, seg_n):
-    """Display the INSAR demo BVP plot."""
-    fig = plot_signal(bvp, 'Timestamp', 'BVP', fs, seg_num, seg_n, 'Peak')
-    fig.update_layout(
-        height = 300,
-        margin = dict(l = 10, r = 10, b = 50, t = 30, pad = 10),
-        yaxis_title = 'nW'
-    )
-    return fig
-
-
