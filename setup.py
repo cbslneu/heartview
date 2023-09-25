83d5452f75e70a322bc75535b3d08c56cfb9dc17
nmy2103 <yamane.n@northeastern.edu>
2023-09-25 11:57:58 -0700

    set up packaging and documentation requirements

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+# Dependencies
+requirements = [
+    'blosc2==2.0.0',
+    'celery',
+    'cython==0.29.21',
+    'dash',
+    'dash_bootstrap_components',
+    'dash_daq',
+    'dash_uploader',
+    'diskcache',
+    'matplotlib',
+    'multiprocess',
+    'numpy',
+    'packaging==21.3',
+    'pandas',
+    'plotly',
+    'psutil',
+    'pyEDFlib',
+    'scipy'
+]
+
+setuptools.setup(
+    name = 'heartview',
+    version = '1.0',
+    author = 'Natasha Yamane, Varun Mishra, and Matthew S. Goodwin',
+    description = 'A signal quality assessment pipeline for wearable cardiovascular data',
+    license = '',
+    packages = setuptools.find_packages(),
+    install_requires = requirements,
+    url = 'https://github.com/cbslneu/heartview',
+    python_requires = '>=3.9'
+)
+
