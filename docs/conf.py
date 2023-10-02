a0e15a10ce065e73bf0695d05c2a7623a25e483d
nmy2103 <yamane.n@northeastern.edu>
2023-10-02 10:58:10 -0400

    removed sphinx-mdinclude extension

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_mdinclude',
-              'sphinx.ext.autodoc',
+extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
               'sphinx.ext.viewcode']
 templates_path = ['_templates']
