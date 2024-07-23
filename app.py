15f6cb95fc64bcfc2cac2f688f6464bbedd4b0e6
nmy2103 <natasha.yamane@gmail.com>
2024-07-22 23:58:28 -0400

    fixed bug in clearing 'temp/' on windows

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from os import environ
+from os import name as os_name
 from dash import Dash, DiskcacheManager, CeleryManager
 from heartview.dashboard.utils import _clear_temp
 from heartview.dashboard.layout import layout
 from heartview.dashboard.callbacks import get_callbacks
 import dash_bootstrap_components as dbc
+
+sep = '\\' if os_name == 'nt' else '/'
 
 if 'REDIS_URL' in environ:
     from celery import Celery
@@ -24,8 +27,8 @@ app = Dash(
 )
 app.title = 'HeartView Dashboard'
 app.layout = layout
-_clear_temp()
 get_callbacks(app)
 
 if __name__ == '__main__':
+    _clear_temp()
     app.run_server()
