75b3b4a4af2949dd0727aded9db766a42cdec7eb
nmy2103 <natasha.yamane@gmail.com>
2025-07-04 01:15:01 -0400

    Update setup metadata and requirements

--- setup.py
+++ setup.py
@@ -2,34 +2,33 @@ import setuptools
 
 # Dependencies
 requirements = [
-    'blosc2==2.0.0',
-    'celery',
     'cython==0.29.21',
-    'dash',
-    'dash_bootstrap_components',
-    'dash_daq',
+    'dash==2.17.1',
+    'dash_bootstrap_components==1.6.0',
+    'dash_daq==0.5.0',
     'dash_uploader',
     'diskcache',
     'matplotlib',
     'multiprocess',
-    'numpy',
+    'numpy==1.26.4',
+    'openpyxl',
     'packaging==21.3',
     'pandas',
     'plotly',
     'psutil',
     'pyEDFlib',
-    'scipy'
+    'scipy',
+    'tqdm==4.65.0'
 ]
 
 setuptools.setup(
     name = 'heartview',
-    version = '1.0',
+    version = '2.0.2',
     author = 'Natasha Yamane, Varun Mishra, and Matthew S. Goodwin',
     description = 'A signal quality assessment pipeline for wearable cardiovascular data',
-    license = '',
+    license = 'GPL-3.0',
     packages = setuptools.find_packages(),
     install_requires = requirements,
     url = 'https://github.com/cbslneu/heartview',
-    python_requires = '>=3.8.6'
+    python_requires = '>=3.9.23'
 )
-
