3b3b3c2ac7156f677510cd4f5c6a8878e196cd3d
nmy2103 <natasha.yamane@gmail.com>
2025-07-04 02:04:36 -0400

    Refactor app.py to remove Celery, reset cache, and clean up path-handling

--- app.py
+++ app.py
@@ -1,23 +1,13 @@
-from os import environ
-from os import name as os_name
-from dash import Dash, DiskcacheManager, CeleryManager
-from heartview.dashboard.utils import _clear_temp
+from pathlib import Path
+from dash import Dash, DiskcacheManager
+from heartview.dashboard.utils import _clear_temp, _make_subdirs
 from heartview.dashboard.layout import layout
 from heartview.dashboard.callbacks import get_callbacks
 import dash_bootstrap_components as dbc
+import diskcache
 
-sep = '\\' if os_name == 'nt' else '/'
-
-if 'REDIS_URL' in environ:
-    from celery import Celery
-    celery_app = Celery(__name__,
-                        broker = environ['REDIS_URL'],
-                        backend = environ['REDIS_URL'])
-    background_callback_manager = CeleryManager(celery_app)
-else:
-    import diskcache
-    cache = diskcache.Cache(f'.{sep}cache')
-    background_callback_manager = DiskcacheManager(cache)
+cache = diskcache.Cache(Path('.') / 'cache')
+background_callback_manager = DiskcacheManager(cache)
 
 app = Dash(
     __name__,
@@ -27,8 +17,11 @@ app = Dash(
 )
 app.title = 'HeartView Dashboard'
 app.layout = layout
+app.server.name = 'HeartView Dashboard'
 get_callbacks(app)
 
 if __name__ == '__main__':
+    _make_subdirs()
     _clear_temp()
+    cache.clear()
     app.run_server()
