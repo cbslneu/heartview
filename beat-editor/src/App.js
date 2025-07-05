e948d31e060d39dbd23f1f47b05c40fb8e4cf836
Tim Lun Chen <32348217+tchen94@users.noreply.github.com>
2025-07-05 03:05:35 -0400

    Revert "Merging dev to main (beat-editor)"

---
+++ beat-editor/src/App.js
@@ -0,0 +1,13 @@
+import PlotBeatSegment from "./components/PlotBeatSegment";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+function App() {
+  return (
+    <div className="container">
+      <PlotBeatSegment />
+    </div>
+  );
+}
+
+export default App;
